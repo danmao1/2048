5d37c0bbf3afded8451f6dd6f3d8e1c891967766
Nick Faddis <nfaddis02@gmail.com>
2021-03-29 14:39:22 -0500

    new

--- Board.java
+++ Board.java
@@ -4,28 +4,19 @@ import java.awt.*;
 public class Board extends JPanel{
     private static final int row = 4;
     private static final int col = 4;
-
-    private JFrame frame;
     
     private Tile nums[][];
 
     public Board(){
-        frame = new JFrame("2048");
-        frame.setPreferredSize(new Dimension(400, 400));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setLayout(new GridLayout(row, col));
+        this.setLayout(new GridLayout(row, col));
         nums = new Tile[col][row];
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                nums[col][row] = new Tile(0);
-                frame.add(nums[col][row]);
+                nums[i][j] = new Tile(0);
+                this.add(nums[i][j]);
             }
         }
-
-        frame.pack();
-        frame.setVisible(true);
     }
 
     public Tile getValue(){
--- Driver.java
+++ Driver.java
@@ -4,5 +4,15 @@ import javax.swing.*;
 public class Driver {
     public static void main(String args[]){
         Board board = new Board();
+
+        JFrame frame = new JFrame("2048");
+        frame.setPreferredSize(new Dimension(400, 400));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(board);
+
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
