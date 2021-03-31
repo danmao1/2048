9d60fe10a5516746ff88b74109bc0afaa6762ac7
Daniel Mao <danielmao@MacBook-Pro-de-Daniel.local>
2021-03-31 21:49:00 +0200

    view

---
+++ view/mainScreenGUI.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainScreenGUI {
+    Board board;
+
+    JFrame frame;
+    JPanel mainPanel;
+    JPanel menuPanel;
+    JButton newGameButton;
+    JButton leaderboardButton;
+
+    public mainScreenGUI(Board board){
+        this.board = board;
+
+        frame = new JFrame("2048");
+        frame.setPreferredSize(new Dimension(400, 450));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainPanel = new JPanel();
+
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        menuPanel = new JPanel();
+        //mainPanel.setBackground(new Color());  //background color of main screen
+        newGameButton = new JButton("New Game");
+        leaderboardButton = new JButton("Leaderboard");
+        menuPanel.add(newGameButton);
+        menuPanel.add(leaderboardButton);
+
+        mainPanel.add(menuPanel);
+        mainPanel.add(board);
+
+        frame.add(mainPanel);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void addNewGameListener(ActionListener l){
+        newGameButton.addActionListener(l);
+    }
+
+    public void addLeaderboardListener(ActionListener l){
+        leaderboardButton.addActionListener(l);
+    }
+}
