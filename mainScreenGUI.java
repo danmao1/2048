5763ab52fdbf34b96b7439c9f07414e838217756
Nick Faddis <nfaddis02@gmail.com>
2021-04-02 16:40:05 -0500

    new

--- mainScreenGUI.java
+++ mainScreenGUI.java
@@ -15,22 +15,35 @@ public class mainScreenGUI {
         this.board = board;
 
         frame = new JFrame("2048");
-        frame.setPreferredSize(new Dimension(400, 450));
+        frame.setPreferredSize(new Dimension(500, 640));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         mainPanel = new JPanel();
-
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
         menuPanel = new JPanel();
-        mainPanel.setBackground(new Color(250, 248, 239));
-        newGameButton = new JButton("New Game");
-        //ImageIcon newGame = new ImageIcon();
-        leaderboardButton = new JButton("Leaderboard");
-        //ImageIcon leaderBoard = new ImageIcon();
+        menuPanel.setLayout(null);
+        menuPanel.setBackground(Color.white);
+        
+        ImageIcon newGame = new ImageIcon("Images/NewGame.png");
+        Image temp1 = newGame.getImage() ;  
+        Image newimg1 = temp1.getScaledInstance(90, 30, java.awt.Image.SCALE_SMOOTH);  
+        newGame = new ImageIcon(newimg1);
+        newGameButton = new JButton(newGame);
+        newGameButton.setBounds(250, 132, 90, 30);
+        
+        ImageIcon leaderboard = new ImageIcon("Images/Leaderboard.png");
+        Image temp2 = leaderboard.getImage() ;  
+        Image newimg2 = temp2.getScaledInstance(90, 30, java.awt.Image.SCALE_SMOOTH);  
+        leaderboard = new ImageIcon(newimg2);
+        leaderboardButton = new JButton(leaderboard);
+
         menuPanel.add(newGameButton);
         menuPanel.add(leaderboardButton);
 
         mainPanel.add(menuPanel);
+
+        board.setPreferredSize(new Dimension(390, 390));
         mainPanel.add(board);
 
         frame.add(mainPanel);
