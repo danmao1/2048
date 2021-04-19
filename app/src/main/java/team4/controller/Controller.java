2542914b9fe6a9ca0d9ecf31a8bad84d20c54e91
Nick Faddis <nfaddis02@gmail.com>
2021-04-19 12:32:14 -0500

    Moved leaderboard calculations to leaderboard class

--- app/src/main/java/team4/controller/Controller.java
+++ app/src/main/java/team4/controller/Controller.java
@@ -43,40 +43,6 @@ public class Controller implements KeyListener{
 	    leaderboard.frame.setVisible(true);
     }
 
-	public void leaderboardCalculation(int score) {
-		ArrayList<String> leader= new ArrayList<String> ();
-		try {
-			File file = new File(getClass().getClassLoader().getResource("scores.txt").getFile());
-			BufferedReader br = new BufferedReader(new FileReader(file)); 
-			FileWriter fw = new FileWriter(file);
-			BufferedWriter bw = new BufferedWriter(fw);
-			String scores, temp; 
-			int j=0;
-			while ((scores = br.readLine()) != null){
-					int i=Integer.parseInt(scores);
-					if (score>i) {
-						String newleader= String.valueOf(score);
-						leader.add(newleader);
-					}
-					leader.add(scores);
-				}
-			while ((scores = br.readLine()) != null){
-					bw.write(leader.get(j));
-					j++;
-				}
-			}
-		 catch (IOException e) {
-			 e.getMessage();
-		 }
-			 
-		}
-		
-			
-		
-
-		
-		
-
     public void showMainScreen(mainScreenGUI ui){
         ui.frame.setVisible(true);
     }
--- app/src/main/java/team4/model/Leaderboard.java
+++ app/src/main/java/team4/model/Leaderboard.java
@@ -2,26 +2,33 @@ package model;
 
 public class Leaderboard {
     
-	/*public void leaderboardCalculation(int score) {
+	/*
+	public void leaderboardCalculation(int score) {
 		ArrayList<String> leader= new ArrayList<String> ();
-		URL text = this.getClass().getClassLoader().getResource("scores.txt");
-		File file = new File(text);
-		BufferedReader br = new BufferedReader(new FileReader(file)); 
-		FileWriter fw = new FileWriter(file);
-        BufferedWriter bw = new BufferedWriter(fw);
-		String scores, temp; 
-		int j=0;
-		while ((scores = br.readLine()) != null){
-				int i=Integer.parseInt(word);
-				if (score>i) {
-					String newleader= String.valueOf(score);
-					leader.add(newleader);
+		try {
+			File file = new File(getClass().getClassLoader().getResource("scores.txt").getFile());
+			BufferedReader br = new BufferedReader(new FileReader(file)); 
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			String scores, temp; 
+			int j=0;
+			while ((scores = br.readLine()) != null){
+					int i=Integer.parseInt(scores);
+					if (score>i) {
+						String newleader= String.valueOf(score);
+						leader.add(newleader);
+					}
+					leader.add(scores);
 				}
-				leader.add(scores);
+			while ((scores = br.readLine()) != null){
+					bw.write(leader.get(j));
+					j++;
+				}
 			}
-		while ((scores = br.readLine()) != null){
-				bw.write(leader[j]);
-				j++;
-			}
-		}*/
+		 catch (IOException e) {
+			 e.getMessage();
+		 }
+			 
+		}
+		*/
 }
