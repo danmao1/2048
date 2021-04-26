d6fdb2a49d2d2e78adcbdd32fab7b4b0e1891b08
Nick Faddis <nfaddis02@gmail.com>
2021-04-26 15:41:53 -0500

    moves test started

--- app/src/test/java/team4/model /MovesTest.java
+++ app/src/test/java/team4/model /MovesTest.java
@@ -1,8 +1,36 @@
 package model;
 
+import view.*;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MovesTest {
-    
+    @Test
+    public void emptyColumnUpTest(){
+        Board board = new Board();
+        BoardGUI gui = new BoardGUI(board);
+        Score score = new Score();
+        Moves mover = new Moves(board, score);
+
+        board.setValue(2, 3, 0);
+        mover.up();
+        
+        assertEquals("The tile should have moved up to the top row", 2, board.getValue(0, 0));
+    }
+
+    @Test
+    public void downTest(){
+        
+    }
+
+    @Test
+    public void leftTest(){
+        
+    }
+
+    @Test
+    public void rightTest(){
+        
+    }
 }
