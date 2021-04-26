d39847373892c6bc319a691d8b9869269f64a144
Daniel Mao <danielmao@MacBook-Pro-de-Daniel.local>
2021-04-26 21:31:52 +0200

    tests

--- app/src/test/java/team4/model /BoardTest.java
+++ app/src/test/java/team4/model /BoardTest.java
@@ -1,33 +1,31 @@
 package model;
 
-import model.*;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
-    Board board;
-
-    @Before
-    public void setUp(){
-        board = new Board();
-    }
+    private Board board;
+	private int count;
+    
 
     @Test
     public void setValueTest(){
+		board = new Board();
         board.setValue(8, 2, 1);
         assertTrue("Value should be 8", board.getValue(2,1) == 8);
     }
 
     @Test
     public void resetTest(){
+		board = new Board();
         board.setValue(2, 0, 1);
         board.setValue(4, 1, 1);
         board.setValue(8, 3, 2);
         board.setValue(16, 2, 0);
         board.reset();
         
-        int count;
+        
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
                 count += board.getValue(i, j);
